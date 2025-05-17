5de6b7fe1364db4e209eaad689da75ce3aaa079b
everl <kolukul1996@gmail.com>
2025-05-17 17:34:34 +0300

    start

--- main.go
+++ main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"net/http"
 )
 
 func main() {
-	token := os.Getenv("TELEGRAM_TOKEN")
-	if token == "" {
-		log.Fatal("TELEGRAM_TOKEN not set")
-	}
-
-	bot, err := tgbotapi.NewBotAPI(token)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет от Docker Go бота!")
-			bot.Send(msg)
-		}
-	}
+	http.ListenAndServe(":8080", nil) // слушаем порт 8080
 }
