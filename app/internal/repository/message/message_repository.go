cbd7032e509313f25f951ac5088ff46aafb20ffd
everl <kolukul1996@gmail.com>
2025-09-17 20:03:25 +0300

    code

--- app/internal/repository/message/message_repository.go
+++ app/internal/repository/message/message_repository.go
@@ -36,15 +36,15 @@ func (mr *MessageRepository) Create(message_id int, from_user int64, text string
 
 func (mr *MessageRepository) GetById(message_id int) *Message {
 	msg := mr.db.QueryRow("SELECT * FROM messages WHERE message_id = ?", message_id)
-	var id, from, send_at int
+	var id, from_user, send_at int
 	var text string
 
-	msg.Scan(&id, &message_id, &from, &text, &send_at)
+	msg.Scan(&id, &message_id, &from_user, &text, &send_at)
 
 	return &Message{
 		ID:        id,
 		MessageId: message_id,
-		FromUser:  from,
+		FromUser:  from_user,
 		Text:      text,
 		SendAt:    send_at,
 	}
